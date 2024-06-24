5542ba737b2b65cd63b3fccc9c6f401844570ae9
Mr4 <dungdv@sabitech.vn>
2024-06-24 23:47:46 +0700

    Fix version function charCodeAt

--- src/Models/VietqrInformation.php
+++ src/Models/VietqrInformation.php
@@ -44,19 +44,19 @@ class VietqrInformation extends Model
         return $result;
     }
 
+    public function charCodeAt($str, $i)
+    {
+        return ord(substr($str, $i, 1));
+    }
+
     public function createCRC($str, $type = VietQrConsts::StandardCRC)
     {
         switch ($type) {
             case VietQrConsts::StandardCRC:
-                function charCodeAt($str, $i)
-                {
-                    return ord(substr($str, $i, 1));
-                }
-
                 $crc = 0xFFFF;
                 $strlen = strlen($str);
                 for ($c = 0; $c < $strlen; $c++) {
-                    $crc ^= charCodeAt($str, $c) << 8;
+                    $crc ^= $this->charCodeAt($str, $c) << 8;
                     for ($i = 0; $i < 8; $i++) {
                         if ($crc & 0x8000) {
                             $crc = ($crc << 1) ^ 0x1021;
